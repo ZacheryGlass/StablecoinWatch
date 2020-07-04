c21ed3f8287246c1183c6ec7a6bbd2ea415148bb
Zachery Glass <ZacheryGlass@pm.me>
2020-07-04 10:48:33 -0500

    Fetch token supply per chain metrics.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const https = require('https');
 const express = require('express');
 const Messari = require('messari-api');
 const MessariClient = new Messari();
@@ -8,12 +9,17 @@ var ethapi = require('etherscan-api').init(ethapikey);
 // CONSTANTS
 const TETHER_DECIMALS = 6;
 const TETHER_CONTRACT_ADDRESS = '0xdac17f958d2ee523a2206206994597c13d831ec7';
+const STABLY_DECIMALS = 6;
+const STABLY_CONTRACT_ADDRESS = '0xa4bdb11dc0a2bec88d24a3aa1e6bb17201112ebe';
 
 // GLOBAL VARS
 let tether_eth_supply = 0;
+let tether_btc_supply = 0;
+let stably_eth_supply = 0;
 let stablecoins = [];
 let totalMCap = 0;
 let totalVolume = 0;
+let totalEthMCap = 0;
 
 // set up express app.
 const app = express();
@@ -26,18 +32,42 @@ updateData();
 cron.schedule('*/10 * * * *', updateData);
 
 async function updateData() {
+    // update tether on eth supply
+    ethapi.stats.tokensupply(null, TETHER_CONTRACT_ADDRESS).then((data) => {
+        tether_eth_supply = data.result / 10 ** TETHER_DECIMALS;
+    });
+
+    // update stably on eth supply
+    ethapi.stats.tokensupply(null, STABLY_CONTRACT_ADDRESS).then((data) => {
+        stably_eth_supply = data.result / 10 ** STABLY_DECIMALS;
+    });
+
+    // update tether on omni supply supply
+    const url = 'https://api.omniexplorer.info/v1/property/31';
+
+    https.get(url, (res) => {
+        res.setEncoding('utf8');
+        let body = '';
+        res.on('data', (data) => {
+            body += data;
+        });
+        res.on('end', () => {
+            body = JSON.parse(body);
+            tether_btc_supply = body.totaltokens;
+        });
+    });
+
     let response = await MessariClient.assets.all({ limit: 500 });
     allCoins = response.data.data;
 
     stablecoins = [];
     totalMCap = 0;
     totalVolume = 0;
+    totalEthMCap = 0;
 
     // update stablecoins data
     allCoins.forEach((coin) => {
         if (coin.profile.sector === 'Stablecoins') {
-            totalMCap += coin.metrics.marketcap.current_marketcap_usd;
-            totalVolume += coin.metrics.market_data.real_volume_last_24_hours;
             let scoin = {
                 name: coin.name,
                 symbol: coin.symbol,
@@ -51,18 +81,20 @@ async function updateData() {
                     coin.metrics.market_data.real_volume_last_24_hours
                 ),
                 volume: coin.metrics.market_data.real_volume_last_24_hours,
+                eth_supply: 0,
+                btc_supply: 0,
             };
+            scoin.eth_supply = supply_on_ethereum(scoin);
+            scoin.btc_supply = scoin.name == 'Tether' ? tether_btc_supply : 0;
 
             stablecoins.push(scoin);
+            totalMCap += coin.metrics.marketcap.current_marketcap_usd;
+            totalVolume += coin.metrics.market_data.real_volume_last_24_hours;
+            totalEthMCap += scoin.eth_supply;
         }
     });
 
-    // update tether on eth supply
-    ethapi.stats.tokensupply(null, TETHER_CONTRACT_ADDRESS).then((data) => {
-        tether_eth_supply = data.result / 10 ** TETHER_DECIMALS;
-    });
-
-    // update tether on eth supply
+    // update tether on tron supply
     //https://www.npmjs.com/package/trongrid
     // tronapi.asset.getList('Tether', options);
 }
@@ -81,6 +113,26 @@ function roundMCap(v) {
     }
 }
 
+function supply_on_ethereum(coin) {
+    switch (coin.symbol) {
+        case 'USDT':
+            return tether_eth_supply;
+            break;
+
+        case 'USDS':
+            return stably_eth_supply;
+            break;
+
+        default:
+            if (coin.type == 'ERC-20') {
+                return coin.mcap;
+            } else {
+                return 0;
+            }
+            break;
+    }
+}
+
 // create home page
 app.get('/', async (req, res) => {
     res.render('home', {
@@ -89,7 +141,8 @@ app.get('/', async (req, res) => {
         totalMCap_s: roundMCap(totalMCap),
         totalVolume: totalVolume,
         totalVolume_s: roundMCap(totalVolume),
-        tetherEthSupply: tether_eth_supply,
+        totalEthMCap: totalEthMCap,
+        totalEthMCap_s: roundMCap(totalEthMCap),
     });
 });
 
