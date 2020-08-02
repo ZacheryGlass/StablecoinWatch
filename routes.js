842dca854e80ace3c943831ab239d3202ce41414
Zachery Glass <ZacheryGlass@pm.me>
2020-08-02 16:19:39 -0500

    Set up platform and coin page routes

--- routes.js
+++ routes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 ---------------------------------------------------------*/
 
 /*-----------------------------------------------
-    Home
+    Home (Coins List)
 -----------------------------------------------*/
 router.get('/', async (req, res) => {
     let eth_data = data.platform_data.find((chain) => chain.name === 'Ethereum');
@@ -25,6 +25,37 @@ router.get('/', async (req, res) => {
         active: 'home',
     });
 }); // home
+
+/*-----------------------------------------------
+    Platforms List
+-----------------------------------------------*/
+router.get('/chains', async (req, res) => {
+    let eth_data = data.platform_data.find((chain) => chain.name === 'Ethereum');
+    res.render('chains', {
+        data: data,
+        totalETHMCap: eth_data.total_mcap,
+        totalETHMCap_s: eth_data.total_mcap_s,
+        active: 'chains',
+    });
+}); // chains
+
+/*-----------------------------------------------
+    Coins
+-----------------------------------------------*/
+router.get('/coins/:symbol', async (req, res) => {
+    const sc = data.stablecoins.find((c) => c.symbol == req.params.symbol);
+    if (sc) res.send(sc);
+    else res.send('error');
+}); // coins
+
+/*-----------------------------------------------
+    Platforms
+-----------------------------------------------*/
+router.get('/platforms/:name', async (req, res) => {
+    const platform = data.platform_data.find((p) => p.name.toLowerCase() == req.params.name.toLowerCase());
+    if (platform) res.send(platform);
+    else res.send('error');
+}); // platforms
 
 /*-----------------------------------------------
     Donate
@@ -38,19 +69,6 @@ router.get('/donate', async (req, res) => {
         active: 'donate',
     });
 }); // donate
-
-/*-----------------------------------------------
-    Chains
------------------------------------------------*/
-router.get('/chains', async (req, res) => {
-    let eth_data = data.platform_data.find((chain) => chain.name === 'Ethereum');
-    res.render('chains', {
-        data: data,
-        totalETHMCap: eth_data.total_mcap,
-        totalETHMCap_s: eth_data.total_mcap_s,
-        active: 'chains',
-    });
-}); // chains
 
 /*---------------------------------------------------------
     EXPORTS
