81f3091e30b472bb77df6bca03720f9f83712c67
Zachery Glass <ZacheryGlass@pm.me>
2020-08-28 22:57:19 -0500

    Add support for circulating supply in QtumInterface

--- apis/platforms/qtum.js
+++ apis/platforms/qtum.js
@@ -1,35 +1,59 @@
-const ENDPOINT = 'http://qtum.info/api';
+let fetch = require('node-fetch');
+class QtumInterface {
+    ENDPOINT = 'http://qtum.info/api';
 
-/*---------------------------------------------------------
-Function:
-        getTokenTotalSupply
-Description:
-        Fetches the the balance at an address for the 
-        specified token
----------------------------------------------------------*/
-async function getTokenBalanceAtAddress(token_contract, address) {
-    const request = `${ENDPOINT}/address/${address}/qrc20-balance-history/${token_contract}?limit=1`;
+    /*---------------------------------------------------------
+    Function:
+            getTokenTotalSupply
+    Description:
+            Fetches the the balance at an address for the 
+            specified token
+    ---------------------------------------------------------*/
+    async getTokenBalanceAtAddress(token_contract_address, address) {
+        const request = `${this.ENDPOINT}/address/${address}/qrc20-balance-history/${token_contract_address}?limit=1`;
 
-    return fetch(request)
-        .then((resp) => resp.json())
-        .then((data) => data.transactions[0].tokens[0].balance / 10 ** data.transactions[0].tokens[0].decimals)
-        .catch(console.error);
-}
+        return fetch(request)
+            .then((resp) => resp.json())
+            .then((data) => data.transactions[0].tokens[0].balance / 10 ** data.transactions[0].tokens[0].decimals)
+            .catch(console.error);
+    } // getTokenBalanceAtAddress
 
-/*---------------------------------------------------------
-Function:
-        getTokenTotalSupply
-Description:
-        Fetches the total supply for token specified
-        by 'contract_address'
----------------------------------------------------------*/
-async function getTokenTotalSupply(contract_address) {
-    const request = `${ENDPOINT}/contract/${contract_address}`;
+    /*---------------------------------------------------------
+    Function:
+            getTokenTotalSupply
+    Description:
+            Fetches the total supply for token specified
+            by 'token_contract_address'
+    ---------------------------------------------------------*/
+    async getTokenTotalSupply(token_contract_address) {
+        const request = `${this.ENDPOINT}/contract/${token_contract_address}`;
 
-    return fetch(request)
-        .then((resp) => resp.json())
-        .then((data) => data.qrc20.totalSupply / 10 ** data.qrc20.decimals)
-        .catch(console.error);
-}
+        return fetch(request)
+            .then((resp) => resp.json())
+            .then((data) => data.qrc20.totalSupply / 10 ** data.qrc20.decimals)
+            .catch(console.error);
+    } // getTokenTotalSupply
 
-exports.getTokenTotalSupply = getTokenTotalSupply;
+    /*---------------------------------------------------------
+    Function:
+            getTokenCirculatingSupply
+    Description:
+            Fetches the circulating supply for token specified
+            by 'token_contract_address'
+    Note:   The parameter 'total_supply' is optional but 
+            prevents an additional API call.
+    ---------------------------------------------------------*/
+    async getTokenCirculatingSupply(token_contract_address, exclude_addresses, total_supply) {
+
+        if (!total_supply) total_supply = await this.getTokenTotalSupply(token_contract_address);
+
+        const exclude_total = await Promise.all(
+            exclude_addresses.map((addr) => this.getTokenBalanceAtAddress(token_contract_address, addr))
+        ).then((exclude_amounts) => exclude_amounts.reduce((a, b) => a + b));
+
+        return total_supply - exclude_total;
+    } // total_supply
+} // PlatformInterface
+
+let q = new QtumInterface();
+// module.exports = Qtum;
