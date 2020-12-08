304d91ca1da9407c65ed20a94efbd6b430ca305c
Zachery Glass <ZacheryGlass@pm.me>
2020-12-08 15:03:01 -0600

    implement additional bnb chain api functions

--- interface/platform/bnb.js
+++ interface/platform/bnb.js
@@ -1,7 +1,10 @@
+global.fetch = require('node-fetch');
+
 /*---------------------------------------------------------
     IMPORTS
 ---------------------------------------------------------*/
 const PlatformInterface = require('./platform_interface');
+const { sleep } = require('../../app/util');
 
 /*---------------------------------------------------------
     CLASS
@@ -32,6 +35,21 @@ class BinanceChainInterface extends PlatformInterface {
             Returns a web link the blockchain explorer. 'address'
             parameter is optional.
     ---------------------------------------------------------*/
+    async rate_limit_fetch(url) {
+        await sleep(1000);  /* Binance-Chain API rate limited to 1 call/sec */
+                            /* TODO: Consider keeping track of last call    */
+                            /* time so we don't have to wait 1s every time  */
+        return fetch(url);
+    } // rate_limit_fetch
+
+
+    /*---------------------------------------------------------
+    Function:
+            getExplorerURL
+    Description:
+            Returns a web link the blockchain explorer. 'address'
+            parameter is optional.
+    ---------------------------------------------------------*/
     getExplorerURL(address) {
         if (address) 
             return `https://explorer.binance.org/address/${address}`;
@@ -39,16 +57,92 @@ class BinanceChainInterface extends PlatformInterface {
             return `https://explorer.binance.org/`;
     } // getExplorerURL
     
-    // /*---------------------------------------------------------
-    // Function:    getTokenBalanceAtAddress
-    // Description: Gets the balance at an address for specified token
-    // ---------------------------------------------------------*/
-    // async getTokenBalanceAtAddress(token_contract_address, address) {
-    //     return fetch(`${this.url}/address/${address}/qrc20-balance-history/${token_contract_address}?limit=1`)
-    //         .then((resp) => resp.json())
-    //         .then((data) => data.transactions[0].tokens[0].balance / 10 ** data.transactions[0].tokens[0].decimals)
-    //         .catch(console.error);
-    // } // getTokenBalanceAtAddress
+
+    /*---------------------------------------------------------
+    Function:    getTokenBalanceAtAddress
+    Description: Wrapper function for getTokenBalanceAtAddressBySymbol
+    ---------------------------------------------------------*/
+    async getTokenBalanceAtAddress(owner_address, address) {
+        return this.getTokenBalanceAtAddressByAddress(owner_address, address);
+            
+    } // getTokenBalanceAtAddress
+
+
+    /*---------------------------------------------------------
+    Function:    getTokenBalanceAtAddressByAddress
+    Description: Get the token balance at the specified address
+    ---------------------------------------------------------*/
+    async getTokenBalanceAtAddressByAddress(owner_address, address) {
+        return this.getSymbolsFromOwnerAddress(owner_address)
+        .then( symbols => {
+            if ( symbols.length  < 1 ) {
+                throw new Error( `BNB API ERROR: getTokenBalanceAtAddressByAddress(): NO TOKEN FOUND FOR ADDRESS ${owner_address}` )
+            } else if ( symbols.length  > 1 ) {
+                throw new Error( `BNB API ERROR: getTokenBalanceAtAddressByAddress(): MORE THAN 1 TOKEN FOUND FOR ADDRESS ${owner_address}` )
+            } else {
+                // this address only owns 1 token, so return get the total supply of that token
+                return symbols[0];
+            }
+        })
+        .then(symbol => {
+            this.getTokenBalanceAtAddressBySymbol(symbol, address)
+        });
+        
+    } // getTokenBalanceAtAddress
+
+
+    /*---------------------------------------------------------
+    Function:    getTokenBalanceAtAddressBySymbol
+    Description: Gets the balance at an address for specified token
+    ---------------------------------------------------------*/
+    async getTokenBalanceAtAddressBySymbol(symbol, address) {
+        return this.rate_limit_fetch(`${this.url}/account/${address}`)
+        .then((resp) => resp.json())
+        .then(data => {
+            let ret = 0;
+            data.balances.forEach( balance => {
+                if (balance.symbol == symbol) 
+                    ret = balance.free;
+            })
+            return ret;
+        })
+            
+    } // getTokenBalanceAtAddressBySymbol
+
+
+    /*---------------------------------------------------------
+    Function:    getTokenTotalSupply
+    Description: Wrapper function for getTokenTotalSupplyByAddress
+    ---------------------------------------------------------*/
+    async getTokenTotalSupply(owner_address) {
+        return this.getTokenTotalSupplyByAddress(owner_address);
+
+    } // getTokenTotalSupplyByAddress
+
+    /*---------------------------------------------------------
+    Function:    getTokenTotalSupplyByAddress
+    Description: Gets the total supply for token specified
+                 by 'owner_address'
+    Note:        Only works if the owner address owns only 1 token
+    ---------------------------------------------------------*/
+    async getTokenTotalSupplyByAddress(owner_address) {
+        return this.getSymbolsFromOwnerAddress(owner_address)
+            .then( symbols => {
+                if ( symbols.length  < 1 ) {
+                    throw new Error( `BNB API ERROR: getTokenTotalSupplyByAddress(): NO TOKEN FOUND FOR ADDRESS ${owner_address}` )
+                } else if ( symbols.length  > 1 ) {
+                    throw new Error( `BNB API ERROR: getTokenTotalSupplyByAddress(): MORE THAN 1 TOKEN FOUND FOR ADDRESS ${owner_address}` )
+                } else {
+                    // this address only owns 1 token, so return get the total supply of that token
+                    return symbols[0];
+                }
+            })
+            .then(symbol => {
+                return this.getTokenTotalSupplyBySymbol(symbol);
+            });
+
+    } // getTokenTotalSupplyByAddress
+
 
     /*---------------------------------------------------------
     Function:    getTokenTotalSupplyBySymbol
@@ -56,7 +150,7 @@ class BinanceChainInterface extends PlatformInterface {
                  by 'symbol'
     ---------------------------------------------------------*/
     async getTokenTotalSupplyBySymbol(symbol) {
-        return fetch(`${url}/tokens?limit=500`)
+        return this.rate_limit_fetch(`${this.url}/tokens?limit=500`)
             .then((resp) => resp.json())
             .then(data => {
                 let total_supply;
@@ -71,8 +165,8 @@ class BinanceChainInterface extends PlatformInterface {
                     throw new Error(`BNB API ERROR: getTokenTotalSupplyBySymbol(): COULD NOT FIND SYMBOL ${symbol}`)
     
             })
-            .catch(console.error);
     } // getTokenTotalSupplyBySymbol
+
 
     /*---------------------------------------------------------
     Function:    getSymbolsFromOwnerAddress
@@ -80,7 +174,7 @@ class BinanceChainInterface extends PlatformInterface {
                 owned/created by the specified address
     ---------------------------------------------------------*/
     async getSymbolsFromOwnerAddress( owner_address ) {
-        return fetch(`${url}/tokens?limit=500`)
+        return this.rate_limit_fetch(`${this.url}/tokens?limit=500`)
             .then((resp) => resp.json())
             .then(data => {
                 let symbols = []; // array bc an address can own more than 1 token
@@ -91,7 +185,6 @@ class BinanceChainInterface extends PlatformInterface {
                 });
                 return symbols;
             })
-            .catch(console.error);
     } // getSymbolsFromOwnerAddress
 
 } // BinanceChainInterface
@@ -100,3 +193,6 @@ class BinanceChainInterface extends PlatformInterface {
     EXPORTS
 ---------------------------------------------------------*/
 module.exports = BinanceChainInterface;
+
+// const BNB = new BinanceChainInterface();
+// BNB.getTokenTotalSupplyByAddress('bnb19v2ayq6k6e5x6ny3jdutdm6kpqn3n6mxheegvj', 'bnb19v2ayq6k6e5x6ny3jdutdm6kpqn3n6mxheegvj').then(console.log);
