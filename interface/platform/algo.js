dab6f5f9da43d4c969dcd6015dbc430377563e42
Zachery Glass <ZacheryGlass@pm.me>
2021-01-11 19:13:40 -0600

    Update algorand API to v2

--- interface/platform/algo.js
+++ interface/platform/algo.js
@@ -7,6 +7,7 @@
 const PlatformInterface = require('./platform_interface');
 const algosdk = require('algosdk');
 const keys = require('../../app/keys');
+const { sleep } = require('../../app/util');
 
 /*---------------------------------------------------------
     CLASS
@@ -15,8 +16,9 @@ class AlgorandInterface extends PlatformInterface {
     /*---------------------------------------------------------
     API Endpoint URL
     ---------------------------------------------------------*/
-    url = 'https://mainnet-algorand.api.purestake.io/ps1';
+    url = 'https://mainnet-algorand.api.purestake.io/idx2';
     client = null;
+    api_calls = [];
 
     /*---------------------------------------------------------
     Function:    constructor
@@ -27,15 +29,47 @@ class AlgorandInterface extends PlatformInterface {
         const token = {
             'X-API-key': keys.algo,
         };
-        this.client = new algosdk.Algod(token, this.url, '');
+        this.client = new algosdk.Indexer(token, this.url, '');
     }
+
+    /*---------------------------------------------------------
+    Function:    checkRateLimit
+    Description: 
+    ---------------------------------------------------------*/
+    async checkRateLimit() {
+        const MILLISECOND = 1;
+        const SECOND = 1000 * MILLISECOND;
+        const MINUTE = 60 * SECOND;
+        const MAX_CALLS_PER_SEC = 1;
+
+        // record the time of the latest API call
+        this.api_calls.unshift( Date.now() );
+
+        if( this.api_calls.length >= MAX_CALLS_PER_SEC) {
+            let ms_since_nth_call = Date.now() - this.api_calls.pop();
+            let ms_to_sleep = 5*SECOND - ms_since_nth_call;
+            ms_to_sleep = Math.max( ms_to_sleep, 0 );
+            
+            if(ms_to_sleep) {
+                console.debug(`Algorand Purestake rate limit hit, sleeping for ${ms_to_sleep}ms`);
+                await sleep( ms_to_sleep );
+            }
+        }
+
+    } /* checkRateLimit */
 
     /*---------------------------------------------------------
     Function:    getAssetInfo
     Description: 
     ---------------------------------------------------------*/
-    async getAssetInfo(asset_id) {
-        return this.client.assetInformation(asset_id);
+    async getAssetInfoByID(id) {
+        try{
+            await this.checkRateLimit();
+            const resp = await this.client.searchForAssets().limit(1).index(id).do();
+            return resp.assets[0];
+        } catch (e) {
+            console.error(e);
+        }
     }
 
     /*---------------------------------------------------------
@@ -43,7 +77,13 @@ class AlgorandInterface extends PlatformInterface {
     Description: 
     ---------------------------------------------------------*/
     async getAccountInfo(address) {
-        return this.client.accountInformation(address);
+        try{
+            await this.checkRateLimit();
+            const resp = await this.client.lookupAccountByID(address).do();
+            return resp.account;
+        } catch (e) {
+            console.error(e);
+        }
     }
 
     /*---------------------------------------------------------
@@ -58,9 +98,8 @@ class AlgorandInterface extends PlatformInterface {
             return 'https://algoexplorer.io/';
         } else {
             return this.getAccountInfo(address).then((info) => {
-                let coin_ids = Object.keys(info.thisassettotal);
-                if (coin_ids.length == 1) {
-                    return `https://algoexplorer.io/asset/${coin_ids[0]}`;
+                if (info.assets.length == 1) {
+                    return `https://algoexplorer.io/asset/${info.assets[0]['asset-id']}`;
                 } else {
                     return `https://algoexplorer.io/address/${address}`;
                 }
@@ -75,14 +114,26 @@ class AlgorandInterface extends PlatformInterface {
     ---------------------------------------------------------*/
     async getTokenTotalSupply(contract_address) {
         let info = await this.getAccountInfo(contract_address);
-        let coin_ids = Object.keys(info.thisassettotal);
+        const assets = info['created-assets'];
 
-        if (coin_ids.length > 1) {
+        if (assets.length > 1) {
             throw `Error: Multiple tokens created by this address. Use getTokenSupplyById instead.`;
         } else {
-            let coin_data = info.thisassettotal[coin_ids[0]];
-            let total_minted = coin_data.total / 10 ** coin_data.decimals;
-            let amount_not_yet_issued = info.assets[coin_ids[0]].amount / 10 ** coin_data.decimals;
+            // get the total number of tokens issued
+            const total_minted = assets[0].params.total / 10 ** assets[0].params.decimals;
+            const asset_id = assets[0].index;
+
+            // check how many tokens are held by the address that created this token.
+            // consider those as not yet issued.
+            let amount_not_yet_issued = 0;
+            if( info.assets.length > 0) {
+                info.assets.forEach( asset => {
+                    if( asset[`asset-id`] == asset_id) {
+                        amount_not_yet_issued = asset.amount / 10 ** assets[0].params.decimals;
+                    }
+                });
+            }
+                
             return total_minted - amount_not_yet_issued;
         }
     }
